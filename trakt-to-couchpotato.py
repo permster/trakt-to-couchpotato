bf36b10023931b0d8001ce7507623041a3f25d66
permster <tjgollih@gmail.com>
2014-09-15 23:54:08 -0700

    Various fixes
    - Added -log argument to specify a log file at the command line.
    - urllib2 exception handling.
    - Added check to make sure Couch Potato API is available.
    - Fixed Couch Potato API changes specific to the category.list method.

--- trakt-to-couchpotato.py
+++ trakt-to-couchpotato.py
@@ -12,6 +12,12 @@ import base64
 import hashlib
 import logging
 import os.path
+import argparse
+
+# argument parser
+parser = argparse.ArgumentParser(description='Import trakt.tv movies into Couch Potato.')
+parser.add_argument('-log', help='log file path')
+args = parser.parse_args()
 
 # declare variables
 trakt_apikey= ''
@@ -20,9 +26,11 @@ username =''
 password = ''
 cp_urls = {
            'base': 'http://localhost:5050/api/',
+           'app_available': '%s/app.available',
            'category_list': '%s/category.list/',
            'movie_add': '%s/movie.add/?identifier=%s&category_id=%s'
            }
+
 trakt_urls = {
         'base': 'https://api.trakt.tv/',
         'watchlist_shows': 'user/watchlist/shows.json/%s/%s',
@@ -49,6 +57,31 @@ trakt_urls = {
 def sha1(text):
     return hashlib.sha1(text).hexdigest()
 
+def IsCPAvailable(url_path):
+    logger.info('Check to see if Couch Potato is available')
+    request = cp_urls['base'] + url_path % cp_apikey
+    
+    try:
+        request = urllib2.Request(request)
+        #request.add_header("Authorization", "Basic %s" % base64.encodestring('%s:%s' % (username, password)))
+        url = urllib2.urlopen(request)
+    except urllib2.HTTPError, err:
+        if err.code == 404:
+            pass
+        else:
+            logger.exception('urllib2 http error code : ' + str(err.code))
+            logger.exception('urllib2 http error reason : ' + str(err.reason))
+            return {"success": False}
+    except urllib2.URLError, err:
+        logger.exception('urllib2 url error reason : ' + str(err.reason))
+        return {"success": False}
+
+    try:
+        return json.load(url)
+    except ValueError, e:
+        logger.exception('Error in Couch Potato API: ' + e)
+        return {"success": False}
+
 def getsetCPData(url_path, param1=None, param2=None):
     logger.info('Calling Couch Potato API')
     if param1 is None and param2 is None:
@@ -65,13 +98,18 @@ def getsetCPData(url_path, param1=None, param2=None):
         if err.code == 404:
             pass
         else:
-            raise
+            logger.exception('urllib2 http error code : ' + str(err.code))
+            logger.exception('urllib2 http error reason : ' + str(err.reason))
+            return {"success": False}
+    except urllib2.URLError, err:
+        logger.exception('urllib2 url error reason : ' + str(err.reason))
+        return {"success": False}
     
     try:
         return json.load(url)
     except ValueError, e:
         logger.exception('Error in Couch Potato API: ' + e)
-        return
+        return {"success": False}
 
 def getTraktData(url_path, add_param=None):
     if add_param is None:
@@ -88,7 +126,12 @@ def getTraktData(url_path, add_param=None):
         if err.code == 404:
             pass
         else:
-            raise
+            logger.exception('urllib2 http error code : ' + str(err.code))
+            logger.exception('urllib2 http error reason : ' + str(err.reason))
+            return
+    except urllib2.URLError, err:
+        logger.exception('urllib2 url error reason : ' + str(err.reason))
+        return
     
     try:
         return json.load(url)
@@ -108,7 +151,12 @@ def postTraktData(url_path, post_data):
         if err.code == 404:
             pass
         else:
-            raise
+            logger.exception('urllib2 http error code : ' + str(err.code))
+            logger.exception('urllib2 http error reason : ' + str(err.reason))
+            return
+    except urllib2.URLError, err:
+        logger.exception('urllib2 url error reason : ' + str(err.reason))
+        return
     
     try:
         return json.load(url)
@@ -122,7 +170,7 @@ def createTraktList(url, data):
         logger.info('Successfully created the "' + data['name'] + '" list has been created --- ' + trakt_result['message'])
     else:
         if 'already exists' in trakt_result['error']:
-            logger.warning('Unable to create the "' + data['name'] + '" list --- ' + trakt_result['error'])
+            logger.info('The "' + data['name'] + '" list already exists --- ' + trakt_result['error'])
         else:
             logger.error('Error creating the "' + data['name'] + '" list --- ' + trakt_result['error'])
             return
@@ -136,7 +184,12 @@ if __name__ == "__main__":
     logger.setLevel(logging.INFO)
     
     # create a file handler
-    handler = logging.FileHandler(os.path.splitext(os.path.basename(__file__))[0] + '.log')
+    if args.log is not None and len(args.log) > 0:
+        logfile = args.log
+    else:
+        #logfile = os.path.splitext(os.path.basename(__file__))[0] + '.log'
+        logfile = os.path.dirname(os.path.realpath(__file__)) + '\\' + os.path.splitext(os.path.basename(__file__))[0] + '.log'
+    handler = logging.FileHandler(logfile)
     handler.setLevel(logging.INFO)
     
     # create a logging format
@@ -146,29 +199,37 @@ if __name__ == "__main__":
     # add the handlers to the logger
     logger.addHandler(handler)
 
+    # check for Couch Potato availability
+    cp_available = IsCPAvailable(cp_urls['app_available'])
+    if cp_available['success'] == True:
+        logger.info('Success, Couch Potato is available.')
+    else:
+        logger.error('Couch Potato does not appear to be available.')
+        sys.exit(2)
+
     # get category data from Couch Potato
-    logger.info('Attempting to retrieve category information from CouchPotato.')
+    logger.info('Attempting to retrieve category information from Couch Potato.')
     cp_categories = getsetCPData(cp_urls['category_list'])
     if cp_categories['success'] == True:
         logger.info('At least one or more categories was found, querying for name and id.')
         category_list = []
-        for rs in cp_categories['list']:
+        for rs in cp_categories['categories']: # changed from 'list' to 'categories' (CPS API change)
             category_list.append({
                              'name': rs['label'],
-                             'id': rs['id']
+                             'id': rs['_id']        # changed from 'id' to '_id' (CPS API change)
                              })
-            logger.info('name:' + rs['label'] + ', id:%d' % rs['id'])
+            logger.info('name:' + rs['label'] + ', id:%s' % rs['_id'])   # changed from 'id' to '_id' and %d to %s for unicode _id value (CPS API change)
         if len(category_list) > 0:
             logger.info('Success, found %d categories.' % len(category_list))
         else:
             logger.error('Unable to build category list from Couch Potato.')
             sys.exit(2)
     else:
-        logger.error('Error retreiving category information. Error: ' + cp_categories['error'])
+        logger.error('Error retrieving category information. Error: ' + cp_categories['error'])
         sys.exit(2)
 
     # create Trakt lists (that don't already exist) for Couch Potato categories
-    logger.info('Attempting to create Trakt lists (that don''t already exist) for Couch Potato categories.')
+    logger.info('Attempting to create Trakt lists (that don\'t already exist) for Couch Potato categories.')
     for category in category_list:
         data = {
                 'username': username,
@@ -180,7 +241,7 @@ if __name__ == "__main__":
         createTraktList(trakt_urls['post_list_add'], data)
         
         # create archive Trakt list (that don't already exist)
-        logger.info('Attempting to create Trakt archive lists (that don''t already exist).')
+        logger.info('Attempting to create Trakt archive lists (that don\'t already exist).')
         data['name'] = data['name'] + '_archive'
         createTraktList(trakt_urls['post_list_add'], data)
 
@@ -204,7 +265,7 @@ if __name__ == "__main__":
     logger.info('Attempting to retrieve list details from Trakt.tv.')
     movies = []
     for category in category_list:
-        logger.info('Retreiving details for the "' + category['name'] + '" list.')
+        logger.info('Retrieving details for the "' + category['name'] + '" list.')
         trakt_list_details = getTraktData(trakt_urls['get_user_list'], category['slug'])
         if len(trakt_list_details) > 0:
             i = 0
@@ -226,7 +287,7 @@ if __name__ == "__main__":
         logger.info('Total movie(s) retrieved: %d.' % len(movies))
     
         # add movies into Couch Potato wanted list
-        logger.info('Attempting to add movie(s) into CouchPotato wanted list.')
+        logger.info('Attempting to add movie(s) into Couch Potato wanted list.')
         for movie in movies:
             cp_result = getsetCPData(cp_urls['movie_add'], movie['imdb_id'], movie['cp_cat_id'])
             if cp_result['success'] == True:
@@ -295,7 +356,10 @@ if __name__ == "__main__":
                             }
                     trakt_result = postTraktData(trakt_urls['post_listitem_add'], data)
                     if trakt_result['status'] == 'success':
-                        logger.info('Success, the movie has been added to the archive list --- %d' % trakt_result['inserted'])
+                        if trakt_result['inserted'] == 0:
+                            logger.info('Success, the movie was already in the archive list --- %d changes made' % trakt_result['inserted'])
+                        else:
+                            logger.info('Success, the movie has been added to the archive list --- added %d' % trakt_result['inserted'])
                     else:
                         logger.error('Error, unable to add the movie to the archive list.')
                 
